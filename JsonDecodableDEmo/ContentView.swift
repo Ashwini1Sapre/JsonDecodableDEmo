7e6e3b093c07fb74b43b21c05e933e259ca2b90a
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-05 15:21:22 +0530

    Initial Commit

---
+++ JsonDecodableDEmo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JsonDecodableDEmo
+//
+//  Created by Knoxpo MacBook Pro on 05/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
